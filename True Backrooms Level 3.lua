c4044c11976d8830ff8b7888698ee1ef3619be59
SnapSkullYT <90359066+SnapSkullYT@users.noreply.github.com>
2022-02-12 15:16:13 -0600

    Add files via upload

---
+++ True Backrooms Level 3.lua
@@ -0,0 +1,10 @@
+--  _                    _   ____  
+-- | |                  | | |___ \ 
+-- | |     _____   _____| |   __) |
+-- | |    / _ \ \ / / _ \ |  |__ < 
+-- | |___|  __/\ V /  __/ |  ___) |
+-- |______\___| \_/ \___|_| |____/ 
+
+game.Workspace.MainDoor.Generator1On.Value = true
+game.Workspace.MainDoor.Generator2On.Value = true
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-210.579697, -15.0099821, 158.324982, 1, 0, 0, 0, 1, 0, 0, 0, 1)
